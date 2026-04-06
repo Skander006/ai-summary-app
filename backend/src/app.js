96384406e43853bbcdae38572627b228427325f5
Skander Saafi <saafiskander1@gmail.com>
2026-04-06 19:26:33 +0200

    Added vercel.json file

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,7 @@ app.get('/health', (req,res)=>{
     res.status(200).json({status : 'ok'});
 });
 
-const PORT = process.env.PORT || 2525;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, ()=>{
     console.log(`Serveur ecoute sur le port ${PORT}...`);
 });
