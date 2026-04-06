55a8110e4b4086abde3aa3379ada3bf6addade89
Skander Saafi <saafiskander1@gmail.com>
2026-04-06 15:52:40 +0200

    Added the routes for different types of summary

--- backend/src/routes/summary.js
+++ backend/src/routes/summary.js
@@ -4,7 +4,7 @@ import Groq from "groq-sdk";
 
 const router = express.Router();
 
-router.post('/', protect, async(req, res)=>{
+router.post('/short', protect, async(req, res)=>{
     const {text} = req.body;
     if(!text) return res.status(400).json({error : "Texte requis !"});
     try{
@@ -14,7 +14,7 @@ router.post('/', protect, async(req, res)=>{
             messages: [
                 {
                     role : "system",
-                    content : "Résume ce texte en moins de 3 phrases et corrige ",
+                    content : "Résume ce texte en moins de 3 phrases en version courte et corrige ",
                 },
                 {
                     role : 'user',
@@ -29,6 +29,63 @@ router.post('/', protect, async(req, res)=>{
     } catch(err){
         console.log("Erreur du serveur Groq !");
         res.status(400).json({error : err.message});
+    }
+});
+
+router.post('/detailed', protect, async(req,res)=>{
+    const {text} = req.body;
+    if(!text){
+        return res.status(400).json({error : "Texte requis !"});
+    }
+    try{
+        const groq = await new Groq({apiKey : process.env.GROQ_API_KEY});
+        const completion = await groq.chat.completions.create({
+            model : "llama-3.3-70b-versatile",
+            messages : [
+                {
+                    role : "system",
+                    content : "Résume ce texte en version détaillée et corrigée",
+                },
+                {
+                    role : "user",
+                    content : text,
+                }
+            ],
+            max_tokens : 500,
+        });
+        const summary = completion.choices[0].message.content;
+        res.json({summary});
+    } catch(err){
+        res.status(400).json({error : err.message});
+    }
+});
+
+
+router.post('/bullet', protect, async(req,res)=>{
+    const {text} = req.body;
+    if(!text){
+        return res.status(400).json({error : "Texte requis !"});
+    }
+    try{
+        const groq = await new Groq({apiKey : process.env.GROQ_API_KEY});
+        const completion = await groq.chat.completions.create({
+            model : "llama-3.3-70b-versatile",
+            messages : [
+                {
+                    role : "system",
+                    content : "Résume ce texte en bullet points, en corrigeant s'il y'a des erreurs."
+                },
+                {
+                    role : "user",
+                    content : text,
+                }
+            ],
+            max_tokens : 500,
+        });
+        const summary = completion.choices[0].message.content;
+        res.json({summary});
+    } catch(error){
+        res.status(400).json({error : error.message});
     }
 });
 
